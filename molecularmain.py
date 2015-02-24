ddd0c6b62f8678c06426d7acb20e3a7d7330507f
Nick <nick@nick-Satellite-P755.(none)>
2015-02-24 14:36:40 -0500

    pushing the updated main file with the fortran caclulations done with aprtner

--- molecularmain.py
+++ molecularmain.py
@@ -1,33 +1,82 @@
+from molecularfunctionsOOP import particleClass
+import molecularPhysicalQuantities as PQ
 import numpy as np
 import matplotlib.pyplot as plt
 import math 
+import time
+from pylab import *
 import mpl_toolkits.mplot3d.axes3d as p3
 #import matplotlib.animation as animation
 
-from moleculardynamicsfinal import fill_init_pos, fill_init_mom, particleClass, calc_force, sum_forces, plotthings, changemom, changepos, checkEnergy, checkMomenta,checkPotential,checkpotentialSUM
-#import moleculardynamicsfinal as mo
 
 #set global constants
 Np=108
 sigma=1
 epsilon=1
 deltat=.0000004
+timesteps=1000
 mass = 1
 dens = 0.85
 temp = 0.9
 
-particles = particleClass(Np, dens, temp)
+#set intital conditions
+particles = particleClass(Np, dens, temp,mass)
+particles.changeForces()
+#create empty arrays for energy, momentum, temp etc.
+energies= np.zeros((amountoftimesteps+1,1),dtype = float)
+momenta=np.zeros((amountoftimesteps+1,3),dtype = float)
+Ekin=np.zeros((amountoftimesteps+1,1),dtype = float)
+temperature=np.zeros((amountoftimesteps+1,1),dtype = float)
+pot=np.zeros((amountoftimesteps+1,1),dtype = float)
+
+energies[0] = particles.checkEnergy ()
+#momenta[0] =particles.checkMomenta()
+pot[0] =particles.checkPotential()
+Ekin[0]=particles.checkKinEnergy()
+temperature[0]=PQ.calc_Temp(particles)
+
+
 print "Starting Energy: ",checkEnergy (Np,particles.momenta,particles.positions,mass,particles.L)
 print "Starting Momentum: ",checkMomenta(particles.momenta)
 
-for i in range(10):
-  particles.momenta=changemom(deltat,particles.momenta,particles.forces)
-  particles.positions=changepos(deltat,particles.momenta,particles.positions,mass, particles.L)
-  particles.forces=sum_forces(particles.positions,particles.forces,Np,particles.L)
-  #print i,"th Energy: ",checkEnergy(Np,particles.momenta,particles.positions,mass,particles.L)
- # print i, "the potenital: ",checkpotentialSUM(particles.positions,Np,particles.L)
-  #print i,"th Momentum: ",checkMomenta(particles.momenta)
-  #plotthings(particles.positions,Np)
+for i in range(timesteps):
+  #starttime= time.time()
+  
+  particles.update(deltat)
+  if i%20 == 0:
+    PQ.Temp_correction(particles,temp)
+  energies[i+1]= particles.checkEnergy()
+  #momenta[i+1]= particles.checkMomenta()
+  Ekin[i+1]=particles.checkKinEnergy()
+  pot[i+1]=particles.checkPotential()
+  temperature[i+1]=PQ.calc_Temp(Particles)
+  #timeremaining= (time.time()-starttime)*(timesteps-i)
+  #PRINT "TIME LEFT: ", timeremaining 
+
+t= np.arange(amountoftimesteps+1)
+targetarray=np.ones((amountoftimesteps+1,1),dtype = float)*temp
+figure()
+title('Simulation of %s particles'%(Np))
+subplot(141)
+title('Kinetic energy')
+plot(t,Ekin)
+subplot(142)
+plot(t, energies)
+title('total energy')
+subplot(143)
+plot(t, pot)
+title('potential energy')
+subplot(144)
+#title('momenta')
+#plot(t,momenta)
+plot(t, temperature)
+plot(t,targetarray)
+title('temperature')
+print ":)"
+show()
+
+
+
 
 print "End Energy: ",checkEnergy (Np,particles.momenta,particles.positions,mass,particles.L)
 print "End Momentum: ",checkMomenta(particles.momenta)
